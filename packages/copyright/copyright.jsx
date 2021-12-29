592de49eeef17365e2b6a54ec91bbf332d882fcb
Kirill Barsukov <kirillsbarsukov@gmail.com>
2021-12-29 13:45:42 -0500

    first commit

---
+++ packages/copyright/copyright.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Typography  from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import '@emotion/styled';
+import '@emotion/react';
+
+
+export function Copyright({
+  websiteUrl = `${location.protocol}//${location.hostname}`,
+  companyName = 'Starter Company',
+  ...props
+}) {
+  const fullYear = new Date().getFullYear();
+
+  return (
+    <Typography variant="body2" color="text.secondary" {...props}>
+      {'Copyright © '}
+      {fullYear}
+      <Link color="inherit" href={websiteUrl}>
+        {companyName}
+      </Link>
+      {`.  All Rights Reserved`}
+    </Typography>
+  );
+}
